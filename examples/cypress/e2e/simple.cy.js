3e369739eccc3562f86ab68da3ca8d80817aa298
Rouaa Ben ayed <112416975+Rouaaba@users.noreply.github.com>
2024-03-27 16:05:50 +0100

    Update simple.cy.js

--- examples/cypress/e2e/simple.cy.js
+++ examples/cypress/e2e/simple.cy.js
@@ -1,9 +1,12 @@
-describe('The Home Page', () => {
-  it('successfully loads', () => {
-    cy.visit('https://testkube.kubeshop.io');
+describe('Check content in Testkube documentation', function() {
+    it('Visit the documentation URL', function() {
+        cy.visit('https://docs.testkube.io/articles/getting-started');
+    });
 
-    cy.contains(
-      'Testkube provides a Kubernetes-native framework for test definition, execution and results'
-    );
-  });
+    it('Check section copy', function() {
+        // Modify the selector based on the actual class or ID of the section containing the copy
+        // For example, if the copy is within a div element with class 'article-content'
+        cy.contains('testkube agent debug', { timeout: 20000 }).should('be.visible');
+
+    });
 });
